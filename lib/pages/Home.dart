c5f7cbbc00bac7f20e85602c01a24168cfcc8d6f
William Boudreault <boudreaultwilliam@gmail.com>
2022-09-17 23:00:45 -0400

    Premiere ebauche

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:guac_a_mole/components/AppButton.dart';
 
 class Home extends StatelessWidget {
@@ -6,11 +7,42 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AppButton(
-          onPressed: () {
-            print('Bien joué ! Même si c\'est Will qui a cliqué.');
-          },
-        ),
+        child: Column(children: [
+          Text(
+            "Whack-a-mole".toUpperCase(),
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "fa-solid-900",
+            ),
+          ),
+          Text(
+            "Every whack matters".toUpperCase(),
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: "fa-solid-900",
+                color: Color.fromARGB(0xff, 0x58, 0xD1, 0xFF)),
+          ),
+          AppButton(
+              onPressed: () {
+                print('Bien joué ! Même si c\'est Will qui a cliqué.');
+              },
+              text: "New Game"),
+          AppButton(
+              onPressed: () {
+                print('Bien joué ! Même si c\'est Will qui a cliqué.');
+              },
+              text: "High Scores"),
+          AppButton(
+              onPressed: () {
+                print('Bien joué ! Même si c\'est Will qui a cliqué.');
+              },
+              text: "Score Validator"),
+          AppButton(
+              onPressed: () {
+                print('Bien joué ! Même si c\'est Will qui a cliqué.');
+              },
+              text: "About"),
+        ]),
       ),
       //backgroundColor
       backgroundColor: Colors.blue[200],
