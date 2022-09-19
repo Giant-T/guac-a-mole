31844c8aa672e51a51b0fa5a235361e21a30e739
William Boudreault <boudreaultwilliam@gmail.com>
2022-09-18 21:29:38 -0400

    Deuxieme ebauche

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -7,42 +7,50 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(children: [
-          Text(
-            "Whack-a-mole".toUpperCase(),
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontFamily: "fa-solid-900",
-            ),
-          ),
-          Text(
-            "Every whack matters".toUpperCase(),
-            style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: "fa-solid-900",
-                color: Color.fromARGB(0xff, 0x58, 0xD1, 0xFF)),
-          ),
-          AppButton(
-              onPressed: () {
-                print('Bien joué ! Même si c\'est Will qui a cliqué.');
-              },
-              text: "New Game"),
-          AppButton(
-              onPressed: () {
-                print('Bien joué ! Même si c\'est Will qui a cliqué.');
-              },
-              text: "High Scores"),
-          AppButton(
-              onPressed: () {
-                print('Bien joué ! Même si c\'est Will qui a cliqué.');
-              },
-              text: "Score Validator"),
-          AppButton(
-              onPressed: () {
-                print('Bien joué ! Même si c\'est Will qui a cliqué.');
-              },
-              text: "About"),
-        ]),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Whack-a-mole".toUpperCase(),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "fa-solid-900",
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 18.0),
+                  child: Text(
+                    "Every whack matters".toUpperCase(),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "fa-solid-900",
+                        fontSize: 20,
+                        color: Color.fromARGB(0xff, 0x58, 0xD1, 0xFF)),
+                  )),
+              AppButton(
+                  onPressed: () {
+                    print('Bien joué ! Même si c\'est Will qui a cliqué.');
+                  },
+                  text: "New Game"),
+              AppButton(
+                  onPressed: () {
+                    print('Bien joué ! Même si c\'est Will qui a cliqué.');
+                  },
+                  text: "High Scores"),
+              AppButton(
+                  onPressed: () {
+                    print('Bien joué ! Même si c\'est Will qui a cliqué.');
+                  },
+                  text: "Score Validator"),
+              AppButton(
+                  onPressed: () {
+                    print('Bien joué ! Même si c\'est Will qui a cliqué.');
+                  },
+                  text: "About"),
+            ]),
       ),
       //backgroundColor
       backgroundColor: Colors.blue[200],
