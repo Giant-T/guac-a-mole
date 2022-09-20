5bf8b7e0a7757eb1647a6f5b32b925f068e95e77
William Boudreault <boudreaultwilliam@gmail.com>
2022-09-19 21:52:44 -0400

    Modification pour le nouveau bouton

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -34,7 +34,9 @@ class Home extends StatelessWidget {
                   onPressed: () {
                     Navigator.pushNamed(context, '/jeu');
                   },
-                  text: "New Game"),
+                  text: "New Game",
+                  borderColor: const Color.fromARGB(0xff, 0x58, 0xD1, 0xFF),
+                  isBold: true),
               AppButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/tableauscore');
