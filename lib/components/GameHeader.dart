77710122c3cd45f9399bfda4242ea898fe005802
Raphael <raphboisvert@live.fr>
2022-09-23 09:56:46 -0400

    Création du Header du jeu, renommé Jeu.dart pour Game.dart (plus consistant)

---
+++ lib/components/GameHeader.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class GameHeader extends StatefulWidget {
+  final int score;
+  final int life;
+  final int bonus;
+  const GameHeader(
+      {required this.score,
+      required this.life,
+      required this.bonus,
+      super.key});
+
+  @override
+  State<GameHeader> createState() => _GameHeader();
+}
+
+class _GameHeader extends State<GameHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        height: 100,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: FractionalOffset.topCenter,
+          end: FractionalOffset.bottomCenter,
+          stops: [0.5, 0.8, 1],
+          colors: [
+            Color.fromARGB(255, 11, 29, 49),
+            Color.fromARGB(150, 11, 29, 49),
+            Color.fromARGB(0, 11, 29, 49)
+          ],
+        )),
+        child: Row(
+          children: <Widget>[
+            const Spacer(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "BONUS",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "fa-solid-900",
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
+                ),
+                Text(
+                  widget.bonus.toString(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "fa-solid-900",
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "SCORE",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "fa-solid-900",
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
+                ),
+                Text(
+                  widget.score.toString(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "fa-solid-900",
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "LIFE",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "fa-solid-900",
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
+                ),
+                Text(
+                  widget.life.toString(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "fa-solid-900",
+                    color: Color.fromARGB(0xff, 0x58, 0xD1, 0xFF),
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:guac_a_mole/pages/Loading.dart';
 import 'package:guac_a_mole/pages/Home.dart';
-import 'package:guac_a_mole/pages/Jeu.dart';
+import 'package:guac_a_mole/pages/Game.dart';
 import 'package:guac_a_mole/pages/GameOver.dart';
 import 'package:guac_a_mole/pages/About.dart';
 import 'package:guac_a_mole/pages/PageScore.dart';
 import 'package:guac_a_mole/pages/TableauScore.dart';
 import 'package:guac_a_mole/pages/ValidationScore.dart';
 
-
-
-void main() => runApp(MaterialApp(
-  initialRoute: '/home',
-  routes: {
-    '/': (context) => Loading(),
-    '/home': (context) => Home(),
-    '/jeu': (context) => Jeu(),
-    '/gameover': (context) => GameOver(),
-    '/about': (context) => About(),
-    '/score': (context) => PageScore(),
-    '/tableauscore': (context) => TableauScore(),
-    '/validationscore': (context) => ValidationScore(),
-  }
-));
-
+void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/game': (context) => Game(),
+      '/gameover': (context) => GameOver(),
+      '/about': (context) => About(),
+      '/score': (context) => PageScore(),
+      '/tableauscore': (context) => TableauScore(),
+      '/validationscore': (context) => ValidationScore(),
+    }));
