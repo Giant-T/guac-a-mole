1151499daabeb8df610a68e6ba9a522208d5a02c
William Boudreault <boudreaultwilliam@gmail.com>
2022-09-23 12:23:01 -0400

    1ere ebauche

--- lib/components/Background.dart
+++ lib/components/Background.dart
@@ -3,19 +3,27 @@ import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 class Background extends StatelessWidget {
   final Widget child;
+  final Color color;
 
-  const Background({Key? key, required this.child}) : super(key: key);
+  const Background(
+      {Key? key,
+      required this.child,
+      this.color = const Color.fromARGB(229, 11, 29, 49)})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Svg svg = const Svg('images/backgroundPattern.svg', size: Size(50, 50));
+
     return Stack(
       alignment: Alignment.center,
       fit: StackFit.expand,
       children: [
-        const Positioned.fill(
+        Positioned.fill(
             child: Image(
-          image: Svg('images/backgroundPattern.svg',
-              color: Color.fromARGB(229, 11, 29, 49), size: Size(50, 50)),
+          image: svg,
+          color: color,
+          colorBlendMode: BlendMode.srcATop,
           repeat: ImageRepeat.repeat,
         )),
         child
