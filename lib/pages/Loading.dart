b0f2833aa280928b200a1dcd572d8f1cb78184c2
William Boudreault <boudreaultwilliam@gmail.com>
2022-09-23 09:01:08 -0400

    premiere ebauche'

--- lib/pages/Loading.dart
+++ lib/pages/Loading.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:guac_a_mole/components/AppButtonSonarDonut.dart';
+import 'package:guac_a_mole/components/Background.dart';
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[200],
-      body: Center(
-        child: Text("Loading"),
+      backgroundColor: const Color.fromARGB(0xff, 11, 29, 49),
+      body: Background(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AppButtonSonarDonut(
+                  size: 180, color: Colors.white, onPressed: () {}),
+              Text("Loading Awesome stuff".toUpperCase(),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "fa-solid-900",
+                    color: Colors.white,
+                    fontSize: 25,
+                  ))
+            ]),
       ),
     );
   }
