80bcc9b852ffac6243a67f06ce4e39ba6f356c14
William Boudreault <boudreaultwilliam@gmail.com>
2022-09-29 20:36:19 -0400

    Deuxieme ebauche

--- lib/components/AppButton.dart
+++ lib/components/AppButton.dart
@@ -18,7 +18,24 @@ class AppButton extends StatefulWidget {
   State<AppButton> createState() => _AppButtonState();
 }
 
-class _AppButtonState extends State<AppButton> {
+class _AppButtonState extends State<AppButton> with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200));
+    _controller.stop();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -27,50 +44,56 @@ class _AppButtonState extends State<AppButton> {
         margin: const EdgeInsets.only(bottom: 20),
         height: 75,
         width: screenWidth * 0.75,
-        child: Material(
-            shape: RoundedRectangleBorder(
-                side: BorderSide(
-                    color: widget.highlightColor,
-                    width: 2,
-                    style: BorderStyle.solid),
-                borderRadius: BorderRadius.circular(50)),
-            color: Colors.transparent,
-            child: InkWell(
-              splashColor: Colors.transparent,
-              highlightColor: widget.highlightColor,
-              customBorder: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)),
-              onTap: () {
-                widget.onPressed();
-              },
-              onTapDown: (TapDownDetails details) {
-                setState(() {
-                  widget.pressed = true;
-                });
-              },
-              onTapCancel: () {
-                setState(() {
-                  widget.pressed = false;
-                });
-              },
-              onTapUp: (details) {
-                setState(() {
-                  widget.pressed = false;
-                });
-              },
-              child: Center(
-                child: Text(
-                  widget.text.toUpperCase(),
-                  style: TextStyle(
-                      fontFamily: 'fa-solid-900',
-                      fontWeight:
-                          widget.isBold ? FontWeight.bold : FontWeight.normal,
-                      fontSize: 20,
-                      color: widget.pressed
-                          ? const Color.fromARGB(0xff, 11, 29, 49)
-                          : Colors.white),
-                ),
-              ),
-            )));
+        child: ScaleTransition(
+            scale: Tween<double>(begin: 1, end: 1.1).animate(_controller),
+            child: Material(
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                        color: widget.highlightColor,
+                        width: 2,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(50)),
+                color: Colors.transparent,
+                child: InkWell(
+                  splashColor: Colors.transparent,
+                  highlightColor: widget.highlightColor,
+                  customBorder: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  onTap: () {
+                    widget.onPressed();
+                  },
+                  onTapDown: (TapDownDetails details) {
+                    _controller.forward(from: 0);
+                    setState(() {
+                      widget.pressed = true;
+                    });
+                  },
+                  onTapCancel: () {
+                    _controller.reset();
+                    setState(() {
+                      widget.pressed = false;
+                    });
+                  },
+                  onTapUp: (details) {
+                    _controller.reset();
+                    setState(() {
+                      widget.pressed = false;
+                    });
+                  },
+                  child: Center(
+                    child: Text(
+                      widget.text.toUpperCase(),
+                      style: TextStyle(
+                          fontFamily: 'fa-solid-900',
+                          fontWeight: widget.isBold
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          fontSize: 20,
+                          color: widget.pressed
+                              ? const Color.fromARGB(0xff, 11, 29, 49)
+                              : Colors.white),
+                    ),
+                  ),
+                ))));
   }
 }
