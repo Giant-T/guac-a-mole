14066e7d0af2d4807a4abd282eb73d1b1685c123
William Boudreault <boudreaultwilliam@gmail.com>
2022-10-20 19:06:31 -0400

    Get des scores v1

--- lib/pages/TableauScore.dart
+++ lib/pages/TableauScore.dart
@@ -3,54 +3,59 @@ import 'package:guac_a_mole/components/ListScore.dart';
 import 'package:guac_a_mole/components/AppButton.dart';
 import 'package:guac_a_mole/components/Background.dart';
 import 'package:guac_a_mole/models/Score.dart';
+import 'package:guac_a_mole/database/Score_database.dart';
 
 class TableauScore extends StatelessWidget {
-  final List<Score> data = <Score>[
-    Score(1, "time", "name", 100),
-    Score(2, "time", "name2", 99),
-  ];
+  List<Score> data = [];
+
+  Future<void> getData() async {
+    data = await ScoreDatabase.scores();
+    print(data);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color.fromARGB(0xff, 11, 29, 49),
-        body: Background(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 52),
-                    child: Text(
-                      "Highscores".toUpperCase(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "fa-solid-900",
-                        color: Colors.white,
-                        fontSize: 28,
+    return FutureBuilder(
+        future: getData(),
+        builder: ((context, snapshot) => Scaffold(
+            backgroundColor: const Color.fromARGB(0xff, 11, 29, 49),
+            body: Background(
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 52),
+                        child: Text(
+                          "Highscores".toUpperCase(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "fa-solid-900",
+                            color: Colors.white,
+                            fontSize: 28,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    Expanded(flex: 4, child: ListScore(data: data)),
+                    Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 18, bottom: 18),
+                          child: AppButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            text: "Back to menu",
+                            isBold: true,
+                            highlightColor:
+                                const Color.fromARGB(0xff, 88, 209, 255),
+                          ),
+                        )),
+                  ],
                 ),
-                Expanded(flex: 4, child: ListScore(data: data)),
-                Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 18, bottom: 18),
-                      child: AppButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        text: "Back to menu",
-                        isBold: true,
-                        highlightColor:
-                            const Color.fromARGB(0xff, 88, 209, 255),
-                      ),
-                    )),
-              ],
-            ),
-          ),
-        ));
+              ),
+            ))));
   }
 }
